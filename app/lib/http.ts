8e0d51ecf9bf45cbe8e64f86e1c5f455c9443a70
melenaos <menelaosvergis@gmail.com>
2025-12-15 10:44:14 +0200

    Use server url at http calling function.

--- app/lib/http.ts
+++ app/lib/http.ts
@@ -1,10 +1,33 @@
+import {config} from '~/config'
+
+/**
+ * Centralized HTTP client for the application.
+ *
+ * All HTTP requests in this application MUST go through this function.
+ * Do NOT use `fetch` directly anywhere else.
+ *
+ * The server base URL is automatically applied to all relative requests
+ * 
+ * Usage:
+ *   http<T>("/api/resource");
+ *   http<T>("/api/resource", { method: "POST", body: JSON.stringify(data) });
+ */
 export async function http<T>(
   input: RequestInfo,
   init?: RequestInit
 ): Promise<T> {
-  const res = await fetch(input, {
+
+  // prepend Server url if missing
+  const url =
+    typeof input === "string" && !input.startsWith("http")
+    ? `${config.serverUrl}${input.startsWith("/") ? "" : "/"}${input}`
+    : input;
+
+
+  const res = await fetch(url, {
     headers: {
       "Content-Type": "application/json",
+      ...init?.headers,
     },
     ...init,
   });
