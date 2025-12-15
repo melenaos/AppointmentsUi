cdc25434cc715231bed28243f288a6b554f1a8a9
melenaos <menelaosvergis@gmail.com>
2025-12-15 13:56:49 +0200

    add some margin to the success message

--- app/routes/appointments.tsx
+++ app/routes/appointments.tsx
@@ -17,6 +17,7 @@ export default function Appointments() {
 
   return (
     <div className="container mx-auto max-w-lg my-10">
+      
       <AppointmentForm
         onSubmit={create.mutate}
         loading={create.isPending}
